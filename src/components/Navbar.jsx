f90f03646707d0e59baeb6e7e88d441b9349898b
Goobergreve09 <gregory.greve@yahoo.com>
2024-06-06 00:47:30 -0400

    added current date and june title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,40 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Navbar, Nav
- } from 'react-bootstrap';
+import { Container, Navbar, Nav, Row, NavbarText } from 'react-bootstrap';
 import ggpLogo from '../assets/images/ggpLogo.png';
-
+import dayjs from 'dayjs';
 
 export default function Navigation() {
   const [expanded, setExpanded] = useState(false);
+  const date = dayjs().format('dddd, MMMM D, YYYY');
 
   const handleNavbarToggle = () => {
     setExpanded(!expanded);
   };
 
   const handleNavLinkClick = () => {
-    setExpanded(false); 
+    setExpanded(false);
   };
 
   return (
     <>
-      <Navbar
-        expand="lg"
-        expanded={expanded}
-        className="navbar"
-      >
+      <Navbar expand="lg" expanded={expanded} className="navbar">
         <Container fluid>
           <Navbar.Brand href="/">
-            <img
-              src={ggpLogo}
-              alt="Main Logo"
-              width="175"
-              height="75"
-             
-            />
+            <img src={ggpLogo} alt="Main Logo" width="175" height="75" />
           </Navbar.Brand>
-          
           <Navbar.Toggle onClick={handleNavbarToggle} />
           <Navbar.Collapse>
-            <Nav className="navbar">
+            <Nav className="mr-auto navbar">
               <Nav.Link as={Link} to="/Gallery" onClick={handleNavLinkClick}>
                 Gallery
               </Nav.Link>
             </Nav>
+            <NavbarText className="navbar-text p-0">{date}</NavbarText>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
   );
 }
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,22 @@
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container} from "react-bootstrap";
 
 import background from "../assets/images/background.jpg"
+import mensRibbon from "../assets/images/mensRibbon.png"
 
 
 export default function Home() {
+
+
   return (
-    <Container fluid 
-    className="titleContainer"
-    >
-      <Row>
+    <Container fluid className="titleContainer">
+      <Row className="monthlyTitle justify-content-center">
+        <Col>
+        <h1> June is Men's mental health awareness month.</h1> 
+        </Col>
       </Row>
       <Row className="text-center align-items-center justify-content-center pt-5">
         <Col lg={8} xs={12}>
-          <img
-            src={background}
-            alt="collage of photographer"
-            className="collage"
-          />
+          <img src={background} alt="collage of photographer" className="collage" />
         </Col>
       </Row>
     </Container>
